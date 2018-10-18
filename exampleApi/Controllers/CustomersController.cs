c77440e00a0839ce6695d92516bc7c371d8ffc42
Denise Krzyny <denakrz@gmail.com>
2018-10-18 10:42:32 -0300

    corrección + comentarios + Get

--- exampleApi/Controllers/CustomersController.cs
+++ exampleApi/Controllers/CustomersController.cs
@@ -10,29 +10,47 @@ using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
+    [Route("api/[controller]")] // el route me da un URI para invocar a traves de una petitud HTTP los recursos que me da la API.
+    [ApiController]  // lo hace solo el framework gracias a route("api/[controller])
 
-    public class CustomersController : ControllerBase
+    public class CustomersController : ControllerBase // nombre del controller, el framework busca esta clase para modificar el [/controller]
     {
         public readonly string Name;
 
-        [HttpPost]
-        public ActionResult<Customer> CreateCustomer([FromForm]Customer customer)
+        [HttpPost]  // atributo necesario para que el metodo se ejecute siempre y cuando la petitud HTTP respete la URI y el METODO 
+        public ActionResult<Customer> CreateCustomer([FromForm]Customer customer) // serialización, si respeto la ruta y el metodo, se acciona esto
         {
 
-         if (customer.Name == null)
+
+            if (string.IsNullOrEmpty(customer.Name)) // si customer está vacío o nulo
             {
-                return BadRequest(customer);
+                return BadRequest(customer); // retorna 400
             }
-            else
+            else // sino
             {
-                AddCustomer(customer);
-                return Ok(customer);
+                AddCustomer(customer); // agrega customer
+                return Ok(customer); // retorna 200
             }
         }
 
-        private void AddCustomer(Customer customer)
+        [HttpGet]
+
+        public ActionResult<Customer> GetCustomer()
+        {
+            Customer customer = new Customer(); // construyo un nuevo costumer
+            customer.Name = "Denise"; // agrego valor nombre
+            return Ok(customer); // retorna 200
+
+            /* Constructor dinámico: solo cuando se pueden setear atributos publicos, se crea un constructor en customer.cs 
+            Customer customer = new Costumer ();
+            {
+                Name = "Horacio";
+            };
+            return Ok(customer);
+             */
+        }
+
+        private void AddCustomer(Customer customer) // metodo que guarda en el customer
         {
             var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 
